98efa159555bc7bc827ef252ebb9cd6ae66bd41f
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-01-06 14:09:04 -0800

    Adding Comments to whole Controller file

--- Twiter Clone/ViewController.swift
+++ Twiter Clone/ViewController.swift
@@ -19,31 +19,51 @@ class ViewController: UIViewController, UITableViewDataSource {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    //create my account Store
     let myTwitterAccountStore = ACAccountStore()
+    //Of Type Twitter
     let myTwitterAccountType = myTwitterAccountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
     
+    //request access but before make sure to set emulator to be able to access in the settings - CTRL - Shift H
     myTwitterAccountStore.requestAccessToAccountsWithType(myTwitterAccountType, options: nil) { (granted : Bool, error : NSError!) -> Void in
+      //if account is validated
       if granted {
         let accounts = myTwitterAccountStore.accountsWithAccountType(myTwitterAccountType)
         if !accounts.isEmpty {
+          //if there is actually an account
           let myTwitterAccount = accounts.first as ACAccount
           
-          //bad account for testing
+          //bad url for testing bad request codes- missing last 'n'
           let requestURL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.jso")
           
+          
+          // this rest api found here - https://dev.twitter.com/rest/public
+          
           //let requestURL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
+          
+          //sending the service request
           let twitterRequest = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: requestURL, parameters: nil)
           twitterRequest.account = myTwitterAccount
+          
+          //here comes the clousure that performs the 'in' section of code after response has been returned from the server
           twitterRequest.performRequestWithHandler(){ (data, response, error) -> Void in
             switch response.statusCode {
+              
+            //request is succesful
             case 200...299:
               
               if let jsonArray = NSJSONSerialization.JSONObjectWithData(data, options: nil, error:nil) as? [AnyObject] {
                 
                 for object in jsonArray {
                   if let jsonDictionary = object as? [String : AnyObject] {
+                    
+                    //instantiate a Tweet class
                     let tweet = Tweet(jsonDictionary)
+                    
+                    //add my Tweet object to array of tweets declared on top of class
                     self.tweets.append(tweet)
+                    
+                    //this code returns the interface operation to main thread which is the correct way to do this
                     
                     NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
                       
@@ -55,6 +75,14 @@ class ViewController: UIViewController, UITableViewDataSource {
               }
               
             case 300...599:
+              
+              //this is UNDER CONSTRUCTION
+              
+              var errorPointer : NSErrorPointer = nil
+              
+              let badJsonArray = NSJSONSerialization.JSONObjectWithData(data, options: nil, error:nil) as? [AnyObject]
+              
+              println(badJsonArray)
               
               //display my own alert message - TO DO
               let alertController = UIAlertController(title: "Bad Request", message:
@@ -83,13 +111,17 @@ class ViewController: UIViewController, UITableViewDataSource {
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("TWEET_CELL", forIndexPath: indexPath) as CustomTableViewCell
+    
+    //this var name is what gets the value of user form within user dictionary which is within json
     var name = self.tweets[indexPath.row].user["name"] as String
     let tweet = self.tweets[indexPath.row]
     
     //this after adding CustomTableViewCell.swift - Day1
 
     cell.tweetCustomLabel.text = tweet.text + " by " + name
-    println(cell.tweetCustomLabel.text)
+    
+    //setting image background to blue so we can see the image
+    //Would be nice to see the image from the Tweet
     cell.customImage.backgroundColor = UIColor.blueColor()
     
     
