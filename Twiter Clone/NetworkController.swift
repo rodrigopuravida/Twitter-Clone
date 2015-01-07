1382c2b89157dc123044fe1d4a4a2edc97d3a243
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-01-07 13:28:17 -0800

    Created Network Controller class

---
+++ Twiter Clone/NetworkController.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkController.swift
+//  Twiter Clone
+//
+//  Created by Rodrigo Carballo on 1/7/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+import Foundation
+
+class NetworkController {
+  
+}
