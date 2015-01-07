93bb523a1432cdfc145376bdf5b10301157311a4
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-01-07 14:43:06 -0800

    Homework - W1 D3 - Move all of your network code into a network controller, and create a method to fetch the users timeline with a completion handler

--- Twiter Clone/NetworkController.swift
+++ Twiter Clone/NetworkController.swift
@@ -7,7 +7,110 @@
 //
 
 import Foundation
+import Accounts
+import Social
 
 class NetworkController {
   
+  var myTwitterAccount : ACAccount?
+  
+//  var url : String
+//  
+//  init(apiToBeCalled : String) {
+//    self.url = apiToBeCalled
+//  }
+  
+  init() {
+    
+  }
+  
+  func fetchTwitterHomeTimeline( completionHandler : ([Tweet]?, String?) -> ()) {
+  
+    //create my account Store
+    let myTwitterAccountStore = ACAccountStore()
+    //Of Type Twitter
+    let myTwitterAccountType = myTwitterAccountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+    
+    //request access but before make sure to set emulator to be able to access in the settings - CTRL - Shift H
+    myTwitterAccountStore.requestAccessToAccountsWithType(myTwitterAccountType, options: nil) { (granted : Bool, error : NSError!) -> Void in
+      //if account is validated
+      if granted {
+        let accounts = myTwitterAccountStore.accountsWithAccountType(myTwitterAccountType)
+        if !accounts.isEmpty {
+          //if there is actually an account
+          let myTwitterAccount = accounts.first as ACAccount
+          
+          //bad url for testing bad request codes- missing last 'n'
+          //let requestURL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.jso")
+          
+          
+          // this rest api found here - https://dev.twitter.com/rest/public
+          
+          let requestURL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
+          
+          //sending the service request
+          let twitterRequest = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: requestURL, parameters: nil)
+          
+          
+          
+          twitterRequest.account = myTwitterAccount
+          
+          //here comes the clousure that performs the 'in' section of code after response has been returned from the server
+          twitterRequest.performRequestWithHandler(){ (data, response, error) -> Void in
+            switch response.statusCode {
+              
+              //request is succesful
+            case 200...299:
+              
+              
+              if let jsonArray = NSJSONSerialization.JSONObjectWithData(data, options: nil, error:nil) as? [AnyObject] {
+                var tweets = [Tweet]()
+                for object in jsonArray {
+                  if let jsonDictionary = object as? [String : AnyObject] {
+                    
+                    //instantiate a Tweet class
+                    let tweet = Tweet(jsonDictionary)
+                    
+                    //add my Tweet object to array of tweets declared on top of class
+                    tweets.append(tweet)
+                    
+//                    //this code returns the interface operation to main thread which is the correct way to do this
+//                    
+//                    NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+//                      
+//                      self.tweetTableView.reloadData()
+                    //})
+                  }
+                }
+                NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                  completionHandler(tweets, nil)
+                })
+                
+              }
+              
+            case 300...599:
+              
+              println("Twitter HTTP response \(response.statusCode)")
+              
+              
+              //display my own alert message
+              let alertController = UIAlertController(title: "Twitter HTTP response \(response.statusCode)", message:
+                "Please Try Again", preferredStyle: UIAlertControllerStyle.Alert)
+              alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+              
+              //presentViewController(alertController, animated: true, completion: nil)
+              
+            default:
+              println("default case fired")
+            }
+            
+          }
+          
+        }
+      }
+      
+    }
+    
+  }
+
 }
--- Twiter Clone/ViewController.swift
+++ Twiter Clone/ViewController.swift
@@ -13,96 +13,25 @@ import Social
 class ViewController: UIViewController, UITableViewDataSource {
   
   var tweets = [Tweet]()
+  let networkController = NetworkController()
   
   @IBOutlet weak var tweetTableView: UITableView!
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    //create my account Store
-    let myTwitterAccountStore = ACAccountStore()
-    //Of Type Twitter
-    let myTwitterAccountType = myTwitterAccountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
-    
-    //request access but before make sure to set emulator to be able to access in the settings - CTRL - Shift H
-    myTwitterAccountStore.requestAccessToAccountsWithType(myTwitterAccountType, options: nil) { (granted : Bool, error : NSError!) -> Void in
-      //if account is validated
-      if granted {
-        let accounts = myTwitterAccountStore.accountsWithAccountType(myTwitterAccountType)
-        if !accounts.isEmpty {
-          //if there is actually an account
-          let myTwitterAccount = accounts.first as ACAccount
-          
-          //bad url for testing bad request codes- missing last 'n'
-          //let requestURL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.jso")
-          
-          
-          // this rest api found here - https://dev.twitter.com/rest/public
-          
-          let requestURL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
-          
-          //sending the service request
-          let twitterRequest = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: requestURL, parameters: nil)
-          
-          
-          
-          twitterRequest.account = myTwitterAccount
-          
-          //here comes the clousure that performs the 'in' section of code after response has been returned from the server
-          twitterRequest.performRequestWithHandler(){ (data, response, error) -> Void in
-            switch response.statusCode {
-              
-            //request is succesful
-            case 200...299:
-              
-           
-              if let jsonArray = NSJSONSerialization.JSONObjectWithData(data, options: nil, error:nil) as? [AnyObject] {
-                
-                for object in jsonArray {
-                  if let jsonDictionary = object as? [String : AnyObject] {
-                    
-                    //instantiate a Tweet class
-                    let tweet = Tweet(jsonDictionary)
-                    
-                    //add my Tweet object to array of tweets declared on top of class
-                    self.tweets.append(tweet)
-                    
-                    //this code returns the interface operation to main thread which is the correct way to do this
-                    
-                    NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
-                      
-                      self.tweetTableView.reloadData()
-                    })
-                  }
-                }
-          
-              }
-              
-            case 300...599:
-              
-              println("Twitter HTTP response \(response.statusCode)")
-              
-              
-              //display my own alert message
-              let alertController = UIAlertController(title: "Twitter HTTP response \(response.statusCode)", message:
-                "Please Try Again", preferredStyle: UIAlertControllerStyle.Alert)
-              alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
-              
-              self.presentViewController(alertController, animated: true, completion: nil)
-              
-            default:
-              println("default case fired")
-            }
-            
-          }
-          
-        }
-      }
-      
-    }
     
     self.tweetTableView.dataSource = self
-}
+    self.networkController.fetchTwitterHomeTimeline { (tweets, errorString) -> () in
+      if errorString == nil {
+        self.tweets = tweets!
+        self.tweetTableView.reloadData()
+      } else {
+        //show user alert view telling them it didnt work
+      }
+    }
+ 
+  }
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return self.tweets.count
@@ -113,12 +42,12 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     //this var name is what gets the value of user name fromm within user dictionary which is inside json
     let name = self.tweets[indexPath.row].user["name"] as String
-
+    
     let tweet = self.tweets[indexPath.row]
     
     //this after adding CustomTableViewCell.swift - Day1
     //concatenating tweet text + name
-
+    
     cell.tweetCustomLabel.text = tweet.text + " by " + name
     
     //setting image background to blue so we can see the image.  Will show only if no images is present
@@ -129,23 +58,23 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     //variable referencing the image for each tweet
     let tweetImage = self.tweets[indexPath.row].user["profile_image_url"] as String
-  
+    
     let imageURL = NSURL(string: tweetImage)
     
     //adding images by converting NSURL to  data and then to to Image
-      if let data = NSData(contentsOfURL: imageURL!){
-        cell.customImage.contentMode = UIViewContentMode.ScaleAspectFit
-        cell.customImage.image = UIImage(data: data)
-      }
- 
+    if let data = NSData(contentsOfURL: imageURL!){
+      cell.customImage.contentMode = UIViewContentMode.ScaleAspectFit
+      cell.customImage.image = UIImage(data: data)
+    }
+    
     return cell
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  
 }
 
